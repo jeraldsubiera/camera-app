9bb6b9e48ae4240718bff2e22b50f9befa68d1f7
yonisoka <baguiobum@gmail.com>
2023-02-21 14:18:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
-window.onload = function(){ 
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
-}
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-// Start the video stream when the window loads
 
-window.addEventListener("load", cameraStart, false);
+let VIDEO = null;
+let CANVAS = null;
+let CAPTURE = null
+let CONTEXT = null;
+let SCALER = 0.6;
+let SIZE = {x:0, y:0, width:0, height:0};
+
+function main() {
+    CANVAS = document.getElementById("camera--view");
+    CONTEXT = CANVAS.getContext("2d");
+    CAPTURE = document.getElementById("#camera--output");
+    cameraTrigger = document.querySelector("#camera--trigger")
+
+    let promise = navigator.mediaDevices.getUserMedia({video: true});
+
+    promise.then(
+    	function(signal){
+    		VIDEO = document.createElement("video");
+    		VIDEO.srcObject = signal;
+    		VIDEO.play();
+
+    		VIDEO.onloadeddata - function(){
+    			updateCanvas();
+    		}
+
+    	}).catch(function(err){
+    		alert("Camera error: " + err);
+    	}
+    );
+
+   	// Take a picture when cameraTrigger is tapped
+    cameraTrigger.onclick = function() {
+        CONTEXT.width = CANVAS.videoWidth;
+        CONTEXT.height = CANVAS.videoHeight;
+        //cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+        //cameraOutput.src = cameraSensor.toDataURL("image/webp");
+        //cameraOutput.classList.add("taken");
+    };
+};
+
+
+
+function updateCanvas(){
+	CONTEXT.drawImage(VIDEO, 0, 0);
+	window.requestAnimationFrame(updateCanvas);
 }
